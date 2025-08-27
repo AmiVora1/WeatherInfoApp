b7f46525581940f26e4ac0422ffdd39ab70572b9
Ami Vora <ami.vorabe@gmail.com>
2025-08-28 09:57:38 +1200

    Initial commit of weather app with spring boot setup

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String WeatherInfoApp(Model model) {
+        model.addAttribute("message", "Hello, HTML with Spring Boot!");
+        return "WeatherInfoApp"; //maps to WeatherInfoApp.html
+    }
+   
+
+}
