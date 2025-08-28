09974fd145712139bafea9e8b9ecbd4da0c8b5e6
Ami Vora <ami.vorabe@gmail.com>
2025-08-29 11:32:10 +1200

    Exception handling and error page added

--- src/main/java/com/example/demo/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -1,10 +1,5 @@
 package com.example.demo;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
